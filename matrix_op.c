69d46fd2d8897c561cee98d5cd23ed8ce44b452f
nathan11262004-byte <nathan11262004@gmail.com>
2025-12-11 18:32:18 +0800

    Implement matrix operations: add, sub, mult, transpose

--- matrix_op.c
+++ matrix_op.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 #include "matrix_op.h"
+
+//Addition
+void mat_add(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]) {
+    for(int i=0;i<SIZE;i++)
+        for(int j=0;j<SIZE;j++)
+            R[i][j] = A[i][j] + B[i][j];
+}
+
+//Subtraction
+void mat_sub(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]) {
+    for(int i=0;i<SIZE;i++)
+        for(int j=0;j<SIZE;j++)
+            R[i][j] = A[i][j] - B[i][j];
+}
+
+//Element-wise Multiplication
+void mat_elem_mult(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]) {
+    for(int i=0;i<SIZE;i++)
+        for(int j=0;j<SIZE;j++)
+            R[i][j] = A[i][j] * B[i][j];
+}
+
+//Matrix Multiplication
+void mat_mult(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]) {
+    for(int i=0;i<SIZE;i++){
+        for(int j=0;j<SIZE;j++){
+            R[i][j] = 0;
+            for(int k=0;k<SIZE;k++)
+                R[i][j] += A[i][k] * B[k][j];
+        }
+    }
+}
+
+//Transpose
+void mat_transpose(int A[SIZE][SIZE], int R[SIZE][SIZE]) {
+    for(int i=0;i<SIZE;i++)
+        for(int j=0;j<SIZE;j++)
+            R[j][i] = A[i][j];
+}
