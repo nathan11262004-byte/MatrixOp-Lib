5357dacea11335ef79d6cb1c0793186d23148c09
nathan11262004-byte <nathan11262004@gmail.com>
2025-12-11 18:27:19 +0800

    Add matrix operations to matrix_op.h

--- matrix_op.h
+++ matrix_op.h
@@ -1,24 +1,20 @@
 #ifndef MATRIX_OP_H
 #define MATRIX_OP_H
 #define SIZE 3
-typedef double Matrix[SIZE][SIZE];
+
+void mat_add(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]);
+void mat_sub(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]);
+void mat_elem_mult(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]);
+
+void mat_mult(int A[SIZE][SIZE], int B[SIZE][SIZE], int R[SIZE][SIZE]);
+void mat_transpose(int A[SIZE][SIZE], int R[SIZE][SIZE]);
+
+int mat_det(int A[SIZE][SIZE]);
+void mat_adjoint(int A[SIZE][SIZE], int Adj[SIZE][SIZE]);
+
+int mat_inverse(int A[SIZE][SIZE], float Inv[SIZE][SIZE]);
+
+void mat_print_int(int A[SIZE][SIZE]);
+void mat_print_float(float A[SIZE][SIZE]);
+
 #endif
-
-void print_matrix(Matrix A);
-void set_matrix_zero(Matrix C);
-
-void add_matrices(const Matrix A, const Matrix B, Matrix C);
-
-void subtract_matrices(const Matrix A, const Matrix B, Matrix C);
-
-void transpose_matrix(const Matrix A, Matrix C);
-
-void multiply_matrices(const Matrix A, const Matrix B, Matrix C);
-
-void element_wise_multiply(const Matrix A, const Matrix B, Matrix C);
-
-double calculate_determinant(const Matrix A);
-
-void calculate_adjoint(const Matrix A, Matrix C);
-
-int calculate_inverse(const Matrix A, Matrix C);
